f7e223effd2e6d4d06d8c9339e6a905fb1e43a63
Marina8989 <marina.mill209@gmail.com>
2021-06-25 12:21:18 -0700

    react project

--- src/Tour.js
+++ src/Tour.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, name, info, image, price}) => {
-   const [show, setShow] = useState(true)
+   const [show, setShow] = useState(false)
 
     return (
         <article className="single-tour">
-            <img src={image} alt={name} />
+            <img src={image} alt={name} /> 
             <footer>
                 <div className="tour-info">
                     <h4>{name}</h4>
                     <h4 className="tour-price">${price}</h4>
                 </div>
-                <p>{info}</p>
+                <p>{show ? info : info.substring(0, 200)}...
+                 <button onClick={() => {setShow(!show)}}>{show ? 'Show Less' : 'Show More'}</button>
+                </p>
                 <button className="delete-btn">not interested</button>
             </footer>
         </article>
